2c499b3f091cc28ce5a939715b0ce4c0615bd16b
kuzeeexyz <junrelterobias123@gmail.com>
2026-07-03 16:06:50 +0800

    add 3rd number

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function sumCalculator(){
     const n1 = document.getElementById('num1').value ;
     const n2 = document.getElementById('num2').value ;
@@ -18,22 +17,4 @@ function clearInput(){
     document.getElementById('num3').value="";
     document.getElementById('res').textContent="Answer is shown here..";
 
-=======
-function sumCalculator(){
-    const n1 = document.getElementById('num1').value ;
-    const n2 = document.getElementById('num2').value ;
-
-    const number1 = parseFloat(n1);
-    const number2 = parseFloat(n2);
-
-    const sum = number1 + number2;
-    
-    document.getElementById('res').textContent=sum;
-}
-function clearInput(){
-    document.getElementById('num1').value="";
-    document.getElementById('num2').value="";
-    document.getElementById('res').textContent="Answer is shown here..";
-
->>>>>>> 4eda5a4cff35f489c5059c38c3bc72437f318eff
 }
